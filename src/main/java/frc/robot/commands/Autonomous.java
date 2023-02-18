83a7df28e52a0ebfe671d17d57a0de204830d628
OGBlackDiamond <84690305+OGBlackDiamond@users.noreply.github.com>
2023-02-17 21:38:13 -0800

    Created autonomous command

---
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** Class tha runs in the autonomous portion of the game. */
+public class Autonomous extends CommandBase {
+    public Autonomous() {
+        
+    }
+}
