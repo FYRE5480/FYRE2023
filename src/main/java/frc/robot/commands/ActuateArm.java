9dd89c7ade6f1a850665e82341316c6407535858
OGBlackDiamond <84690305+OGBlackDiamond@users.noreply.github.com>
2023-02-20 14:22:16 -0800

    more changes

--- src/main/java/frc/robot/commands/ActuateArm.java
+++ src/main/java/frc/robot/commands/ActuateArm.java
@@ -79,7 +79,9 @@ public class ActuateArm extends CommandBase {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        arm.stopMotor();
+    }
 
     // Returns true when the command should end.
     @Override
